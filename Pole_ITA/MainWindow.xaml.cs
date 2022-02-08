5d664f8ba57ddd79f26b3861c343fe852c9de868
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-02-08 09:09:06 +0100

    Add project files.

---
+++ Pole_ITA/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pole_ITA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int a;
+        int b;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            a = random.Next(1,100);
+            b = random.Next(1, 100);
+            txtVypis.Text = a.ToString() + "; " + b.ToString();
+        }
+    }
+}
